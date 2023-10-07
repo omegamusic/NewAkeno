6f24932fbe3aebb7d40d3781d1986ccceec67e29
Aviax <87578989+TeamAviax@users.noreply.github.com>
2023-10-07 14:34:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamAviax/Aviax/Music",
+    "https://github.com/TeamAviax/AviaxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
