36d20b3d70ab5fa0eaf3ebebd0b2036592d140e0
Aviax <87578989+TeamAviax@users.noreply.github.com>
2023-10-07 14:32:15 +0530

    Update database.py

--- AviaxMusic/utils/database.py
+++ AviaxMusic/utils/database.py
@@ -648,40 +648,40 @@ async def remove_banned_user(user_id: int):
         return
     return await blockeddb.delete_one({"user_id": user_id})
 
-def increase_count(chat, user):
-    user = str(user)
-    today = str(date.today())
-    user_db = chatdb.find_one({"chat": chat})
-
-    if not user_db:
-        user_db = {}
-    elif not user_db.get(today):
-        user_db = {}
-    else:
-        user_db = user_db[today]
-
-    if user in user_db:
-        user_db[user] += 1
-    else:
-        user_db[user] = 1
-
-    #  print(user_db)
-    chatdb.update_one({"chat": chat}, {"$set": {today: user_db}}, upsert=True)
-
-
-name_cache = {}
-
-
-async def get_name(app, id):
-    global name_cache
-
-    if id in name_cache:
-        return name_cache[id]
-    else:
-        try:
-            i = await app.get_users(id)
-            i = f'{(i.first_name or "")} {(i.last_name or "")}'
-            name_cache[id] = i
-            return i
-        except:
-            return id
+#def increase_count(chat, user):
+#    user = str(user)
+#    today = str(date.today())
+#    user_db = chatdb.find_one({"chat": chat})
+#
+#    if not user_db:
+#        user_db = {}
+#    elif not user_db.get(today):
+#        user_db = {}
+#    else:
+#        user_db = user_db[today]
+#
+#    if user in user_db:
+#        user_db[user] += 1
+#    else:
+#        user_db[user] = 1
+#
+ #   #  print(user_db)
+ #   chatdb.update_one({"chat": chat}, {"$set": {today: user_db}}, upsert=True)
+#
+#
+#name_cache = {}
+#
+#
+#async def get_name(app, id):
+#    global name_cache
+#
+#    if id in name_cache:
+#        return name_cache[id]
+#    else:
+#        try:
+#            i = await app.get_users(id)
+#            i = f'{(i.first_name or "")} {(i.last_name or "")}'
+#            name_cache[id] = i
+#            return i
+#        except:
+#            return id
